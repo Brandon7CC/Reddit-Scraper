909c3f793b67a181145d489ab5f1a182b66271de
Brandon Dalton <daltonb@usc.edu>
2017-10-19 20:55:37 -0700

    More structure

--- src/Reddit/PostChild.java
+++ src/Reddit/PostChild.java
@@ -1,5 +1,22 @@
+/*
+ * Brandon Dalton, Christopher Turner
+ * 10/19/2017
+ * University of Southern California
+ * Viterbi School of Engineering
+ * Mass-IG
+ * 
+ * PostChild.java
+ */
 package Reddit;
 
 public class PostChild {
-
+	private String kind;
+	private PostListing data;
+	
+	public String getKind() {
+		return kind;
+	}
+	public PostListing getData() {
+		return data;
+	}
 }
