759f57470a7fca3d9d13d466cebf9d909f5750fe
Christopher Matthew Turner <TurnerC@usc.edu>
2018-03-27 11:46:47 -0700

    Beginning work on some GUIs...

---
+++ src/GUI/GUI.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GUI {
+
+	@FXML
+	private Button startButton;
+
+	@FXML
+	private Button stopButton;
+
+	@FXML
+	private Button restartButton;
+	
+}
